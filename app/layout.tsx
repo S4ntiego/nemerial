d32927f4866608a46fc8463ed32f06f9416b5a2b
S4ntiego <adamksiazek123@gmail.com>
2023-07-29 20:43:37 +0200

    enabled system theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         )}
       >
               <div className="pointer-events-none absolute inset-0 overflow-hidden"><div className="h-full bg-[url('https://res.cloudinary.com/delba/image/upload/h_500/bg_gradient_pfosr9')] bg-top bg-no-repeat opacity-[0.3] will-change-transform"></div></div>
-        <ThemeProvider attribute="class" defaultTheme="dark">
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         <svg className="pointer-events-none fixed isolate z-50 opacity-70 mix-blend-soft-light" width="100%" height="100%">
                     <filter id="pedroduarteisalegend">
                         <feTurbulence type="fractalNoise" baseFrequency="0.80" numOctaves="4" stitchTiles="stitch"></feTurbulence>
